d2ebb4386f6991bb51feb0bf00621ba93f082beb
princ <princ@AUTUMNLEAF>
2025-11-27 12:36:21 +0100

    I lost the plot

--- client/client.js
+++ client/client.js
@@ -6,11 +6,30 @@ const ctxGame = gameCanvas.getContext("2d");
 document.oncontextmenu = function () {
     return false;
 }
+let socket;
+let blob;
+let zoom = 1;
+let height = 6;
+let width = 6;
+let blobs = [];
 
 document.addEventListener("DOMContentLoaded", function () {
    canvas.width= gameCanvas.width = document.body.clientWidth;
     canvas.height= gameCanvas.height = document.body.clientHeight;
     drawGridLines();
+    socket = io.connect("http://localhost:3000/");
+
+    let blob = new Blob(random(width), random(height), Math.floor(Math.random() * 24) + 8);
+    let data = {
+        x: blob.pos.x,
+        y: blob.pos.y,
+        r: blob.r
+    };
+    socket.emit('start', data);
+
+    socket.on('heartbeat', function(data) {
+        blobs = data;
+    });
 });
 
 let x = 400, y = 300;
@@ -32,10 +51,9 @@ gameCanvas.addEventListener("mousemove", e => {
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;
 });
-loop();
+//loop();
 
-function drawGridLines()
-{
+function drawGridLines() {
 
     // canvas.style.border = "1px solid red"
     ctx.beginPath();
@@ -61,8 +79,46 @@ function drawGridLines()
 }
 
 
-socket = io();
+function draw() {
+    background(0);
+    console.log(blob.pos.x, blob.pos.y);
 
+    translate(width / 2, height / 2);
+    let newzoom = 64 / blob.r;
+    zoom = lerp(zoom, newzoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y);
+
+    for (let i = blobs.length - 1; i >= 0; i--) {
+        let id = blobs[i].id;
+        if (id.substring(2, id.length) !== socket.id) {
+            fill(0, 0, 255);
+            ellipse(blobs[i].x, blobs[i].y, blobs[i].r * 2, blobs[i].r * 2);
+
+            fill(255);
+            textAlign(CENTER);
+            textSize(4);
+            text(blobs[i].id, blobs[i].x, blobs[i].y + blobs[i].r);
+        }
+        // blobs[i].show();
+        // if (blob.eats(blobs[i])) {
+        //   blobs.splice(i, 1);
+        // }
+    }
+
+    blob.show();
+    if (mouseIsPressed) {
+        blob.update();
+    }
+    blob.constrain();
+
+    let data = {
+        x: blob.pos.x,
+        y: blob.pos.y,
+        r: blob.r
+    };
+    socket.emit('update', data);
+}
 socket.on("connect", () => {
     console.log(socket.id);
 })
--- server/server.js
+++ server/server.js
@@ -15,8 +15,48 @@ const io = new Server(httpServer, { /* options */ });
 //Test console.log
 console.log(process.env);
 
+
+//Variables
+let blobs = [];
+
+function Blob(id, x, y, r) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.r = r;
+}
+
+setInterval(heartbeat, 33);
+
+function heartbeat() {
+    io.sockets.emit('heartbeat', blobs);
+}
+
 io.on('connection', function (socket) {
-    console.log("Somebody connected!");
+    console.log('We have a new client: ' + socket.id);
+
+    socket.on('start', function(data) {
+        console.log(socket.id + ' ' + data.x + ' ' + data.y + ' ' + data.r);
+        let blob = new Blob(socket.id, data.x, data.y, data.r);
+        blobs.push(blob);
+    });
+
+    socket.on('update', function(data) {
+        //console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+        let blob;
+        for (let i = 0; i < blobs.length; i++) {
+            if (socket.id === blobs[i].id) {
+                blob = blobs[i];
+            }
+        }
+        blob.x = data.x;
+        blob.y = data.y;
+        blob.r = data.r;
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Client has disconnected');
+    });
 
 });
 
