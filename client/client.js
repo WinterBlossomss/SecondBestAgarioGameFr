07af582bdc335e628ab6293e10e038818db2bd7f
Camelia Ciocan <camelia-carmen.ciocan@oebb.at>
2025-11-27 13:35:50 +0100

    commit

--- client/client.js
+++ client/client.js
@@ -3,6 +3,11 @@ const ctx = canvas.getContext("2d");
 const gameCanvas = document.getElementById("gameCanvas");
 const ctxGame = gameCanvas.getContext("2d");
 
+const maxX= 5000;
+const maxY= 5000;
+
+let cameraX,cameraY;
+
 document.oncontextmenu = function () {
     return false;
 }
@@ -16,10 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 let x = 400, y = 300;
 
 function loop() {
-
     ctxGame.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-
-
     ctxGame.beginPath();
     ctxGame.arc(x, y, 20, 0, Math.PI * 2);
     ctxGame.fillStyle = "blue";
@@ -31,26 +33,28 @@ gameCanvas.addEventListener("mousemove", e => {
     const rect = gameCanvas.getBoundingClientRect();
     x = e.clientX - rect.left;
     y = e.clientY - rect.top;
+    ctx.scale(10, 10);
 });
 loop();
 
 function drawGridLines()
 {
-
-    // canvas.style.border = "1px solid red"
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "gray";
+    const rows = 50;
+    const cols = 25;
 
-    let offsetX = Math.floor(canvas.width / 50);
-    let offsetY = Math.floor(canvas.height / 25);
 
-    for (let x = offsetX; x < canvas.width; x += offsetX) {
+    let offsetX = Math.floor(canvas.width / rows);
+    let offsetY = Math.floor(canvas.height / cols);
+
+    for (let x = offsetX; x < maxX; x += offsetX) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x,canvas.height);
     }
 
-    for (let y = offsetY; y <canvas.height; y += offsetY) {
+    for (let y = offsetY; y <maxY; y += offsetY) {
         ctx.moveTo(0, y);
         ctx.lineTo(canvas.width, y);
     }
@@ -66,12 +70,3 @@ socket = io();
 socket.on("connect", () => {
     console.log(socket.id);
 })
-
-
-
-
-
-
-
-
-
