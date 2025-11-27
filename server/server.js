24cd98f8aee2d6e5e6b66ac773a6ab0d3bfe0231
princ <princ@AUTUMNLEAF>
2025-11-27 07:05:01 +0100

    Added ENV config file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
+require("dotenv").config();
+
 const express = require('express');
 const app     = express();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const config  = require('./config.json');
 const httpServer = createServer(app);
-
 app.use(express.static(__dirname + '/../client'));
 const io = new Server(httpServer, { /* options */ });
 
+//Test console.log
+console.log(process.env);
 
 io.on('connection', function (socket) {
     console.log("Somebody connected!");
 
 });
 
-const serverPort = config.port;
+const serverPort = process.env.PORT;
 httpServer.listen(serverPort, function() {
     console.log("Server is listening on port " + serverPort);
 });
