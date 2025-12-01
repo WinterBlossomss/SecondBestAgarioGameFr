cf331581f49d88ece93e578a7925520ce13d53f8
princ <princ@AUTUMNLEAF>
2025-12-01 07:02:25 +0100

    I found the plot
    Player circles can be seen on both screens

--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ app.use(express.static('client'));
 
 const io = new Server(httpServer, { /* options */ });
 
-//Test console.log
-console.log(process.env);
-
-
-//Variables
+// Variables
 let blobs = [];
 
 function Blob(id, x, y, r) {
@@ -24,9 +20,12 @@ function Blob(id, x, y, r) {
     this.r = r;
 }
 
+// Send all blobs to all clients ~30 times per second
 setInterval(heartbeat, 33);
 
 function heartbeat() {
+    // For debugging: see how many blobs you are sending
+    // console.log('heartbeat, blobs:', blobs.length);
     io.sockets.emit('heartbeat', blobs);
 }
 
@@ -34,28 +33,41 @@ io.on('connection', function (socket) {
     console.log('We have a new client: ' + socket.id);
 
     socket.on('start', function(data) {
-        console.log(socket.id + ' ' + data.x + ' ' + data.y + ' ' + data.r);
-        let blob = new Blob(socket.id, data.x, data.y, data.r);
+        console.log('start from', socket.id, data);
+        const blob = new Blob(socket.id, data.x, data.y, data.r);
         blobs.push(blob);
+        console.log('blobs after start:', blobs.length);
     });
 
     socket.on('update', function(data) {
-        //console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
-        let blob;
+        let blob = null;
+
+        // Find the blob belonging to this socket
         for (let i = 0; i < blobs.length; i++) {
-            if (socket.id == blobs[i].id) {
+            if (socket.id === blobs[i].id) {
                 blob = blobs[i];
+                break;  // stop once found
             }
         }
+
+        // If we didn't find a blob, don't crash
+        if (!blob) {
+            console.warn(`Update for unknown blob: ${socket.id}`, data);
+            return;
+        }
+
         blob.x = data.x;
         blob.y = data.y;
         blob.r = data.r;
     });
 
     socket.on('disconnect', function() {
-        console.log('Client has disconnected');
+        console.log('Client has disconnected:', socket.id);
+        // Remove blob for this client
+        const before = blobs.length;
+        blobs = blobs.filter(b => b.id !== socket.id);
+        console.log(`blobs before: ${before}, after: ${blobs.length}`);
     });
-
 });
 
 const serverPort = process.env.PORT || 5000;
@@ -63,6 +75,10 @@ httpServer.listen(serverPort, function() {
     console.log("Server is listening on port " + serverPort);
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my static website!');
-});
+// If you want to serve the game from /, do NOT override the static index.html.
+// Comment this out if you have client/index.html you want to show:
+//
+// app.get('/', (req, res) => {
+//     res.send('Welcome to my static website!');
+//
+// });
